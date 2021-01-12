20d0d412b9998cd7bae4e16fc97bc950e4ef72e2
Mikaela M <59987624+MikaelaMontaos@users.noreply.github.com>
2021-01-12 15:13:06 -0800

    Add files via upload

---
+++ Singly_Linked_List/delete_node_in_linked_list.c
@@ -0,0 +1,77 @@
+/* 
+Delete elements with the same value as the input argument key
+a = Head -> 1 -> 2 -> 1 -> 3 -> NULL
+delete_node(a, 1)
+
+Output:
+a = Head -> 2 -> 3 -> NULL
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} node;
+
+node *create(void) {
+    printf("Enter linked list numbers separated by spaces: ");
+    int val;
+    node *head, *c, *tmp;
+    head = c = (node*)malloc(sizeof(node));
+    head->next = NULL;
+    do {
+        scanf("%d", &val);
+        tmp = (node*)malloc(sizeof(node));
+        tmp->data = val;
+        tmp->next = c->next;
+        c->next = tmp;
+        c = tmp;
+    }
+    while(getchar() != '\n');
+    return(head);
+}
+
+void print_ll(node* l) {
+    printf("Head");
+    while (l->next != NULL) {
+        printf(" -> ");
+        l = l->next;
+        printf("%d", l->data);
+    }
+    printf(" -> NULL\n");
+}
+
+void delete_node(node *l, int key) {
+    node *tmp = l;
+    node *p, *d = NULL;
+    if(tmp != NULL && tmp->data == key) {
+        l = tmp->next;
+        d = tmp;
+        free(d);
+    }
+    while(tmp != NULL) {
+        p = tmp;
+        tmp = tmp->next;
+        if(tmp == NULL) return;
+        if(tmp->data == key) {
+            p->next = tmp->next;
+            d = tmp;
+            tmp = tmp->next;
+            free(d);
+        }
+    }
+}
+
+int main() {
+    node* l;
+    l = create();
+    print_ll(l);
+    
+    delete_node(l, 1);
+    printf("After delete nodes with the value of 1\n");
+    print_ll(l);
+    
+    return 0;
+}
